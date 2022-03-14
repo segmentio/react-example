b17dafbd1c17839ba3ebcf08210003f7430490b0
Catherine Ligad <kateligad@gmail.com>
2022-03-14 11:55:31 +0100

    NavBar init

--- src/examples/components/NavBar.tsx
+++ src/examples/components/NavBar.tsx
@@ -1,1 +1,52 @@
-export {}
+import React from "react"
+import { Pane, Text, GlobeIcon, GitRepoIcon, BookIcon, majorScale, IconButton } from "evergreen-ui"
+
+const NavButton: React.FC<{icon: typeof GlobeIcon | typeof GitRepoIcon, href: string }> = ({ icon, href }) => (
+    <IconButton 
+        icon={icon} 
+        outline="none"
+        appearance="minimal"
+        is="a"
+        marginRight={majorScale(1)}
+        href={href}
+        target="blank_"
+    />
+)
+
+const Navbar: React.FC = () => (
+    <Pane 
+        display="flex"
+        justifyContent="space-between"
+        background="gray75"
+        height={majorScale(6)}
+        alignItems="center"
+        paddingX={majorScale(3)}
+    >
+        <Pane
+            alignItems="center"
+            display="flex"
+            is="a"
+            href="https://app.segment.com/"
+            target="blank_"
+            textDecoration="none"
+            color="inherit"
+        >
+            <NavButton 
+                icon={GlobeIcon}
+                href="https://app.segment.com/"/> 
+            <Text>Segment</Text>
+        </Pane>
+        <Pane>
+            <NavButton 
+                icon={GitRepoIcon}
+                href="https://app.segment.com/"
+            />
+            <NavButton 
+                icon={BookIcon}
+                href="https://app.segment.com/"
+            />
+        </Pane>
+    </Pane>
+)
+
+export default Navbar
