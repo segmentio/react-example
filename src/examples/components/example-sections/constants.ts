a95bdba621f34c4ec9e7f78ae2c081455f0cb6a2
Catherine Ligad <kateligad@gmail.com>
2022-03-14 15:56:11 +0100

    update constants

--- src/examples/components/example-sections/constants.ts
+++ src/examples/components/example-sections/constants.ts
@@ -10,24 +10,24 @@ const analyticsEventSections = [
     },
     {
         title: "analytics.page()",
-        description: "BLURB",
+        description: "The page call lets you record whenever a user sees a page of your website, along with any optional properties about the page.",
         children: PageExample
     },
     {
         title: "analytics.track()",
-        description: "BLURB",
+        description: "The track call is how you record any actions your users perform, along with any properties that describe the action.",
     },
     {
         title: "analytics.identify()",
-        description: "BLURB",
+        description: "The identify call lets you tie a user to their actions and record traits about them. It includes a unique User ID and any optional traits you know about the user, like their email, name, etc.",
     },
     {
         title: "Putting it all together",
-        description: "BLURB",
+        description: "You can use track and identify to understand who is doing what.",
     },
     {
         title: "More examples: analytics.track()",
-        description: "BLURB",
+        description: "Product added to cart",
     },
     {
         title: "More examples: analytics.identify()",
