c3eebf701a775a95808cf2a30ae88e48487dae27
Catherine Ligad <kateligad@gmail.com>
2022-03-14 17:44:27 +0100

    add analytics quick start initial files

---
+++ src/examples/analytics-quick-start/App.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react"
+import { homePageViewed } from "./analytics"
+import BaseApp from "../shared/BaseApp"
+
+const App: React.FC = () => {
+    useEffect(() => {
+        homePageViewed()   
+    }, [])
+
+    return <BaseApp/>
+}
+
+export default App
