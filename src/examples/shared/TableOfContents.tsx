67a94737217b0e89439835a9a7f3f51c92a79be5
Catherine Ligad <kateligad@gmail.com>
2022-03-15 04:20:31 +0100

    use analytics-quick-start as main example

--- src/examples/shared/TableOfContents.tsx
+++ src/examples/shared/TableOfContents.tsx
@@ -1,14 +1,14 @@
 import React from "react"
 import { Pane, majorScale, Link } from "evergreen-ui"
 import analyticsEventSections from "./example-sections/constants"
-// import { tableOfContentsItemClicked } from "../analytics-quick-start/analytics"
-import useAnalytics from "../analytics-package/useAnalytics"
+import { tableOfContentsItemClicked } from "../analytics-quick-start/analytics"
+// import useAnalytics from "../analytics-package/useAnalytics"
 
 const TableOfContents: React.FC<{
   onContentClick: (index: number) => void
 }> = ({ onContentClick }) => {
   const contents = analyticsEventSections.map((section) => section.title)
-  const { tableOfContentsItemClicked } = useAnalytics()
+  // const { tableOfContentsItemClicked } = useAnalytics()
   return (
     <Pane
       display="flex"
