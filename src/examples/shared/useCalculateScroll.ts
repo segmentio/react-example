27d5959d4f8cd2c545d5c1996cfd0b5d56923287
Catherine Ligad <kateligad@gmail.com>
2022-05-12 12:52:00 -0700

    cleanup from feedback

--- src/examples/shared/useCalculateScroll.ts
+++ src/examples/shared/useCalculateScroll.ts
@@ -1,46 +1,33 @@
 import { useLayoutEffect, useState } from "react"
 import analyticsEventSections from "./example-sections/constants"
 import { majorScale } from "evergreen-ui"
-
-const clamp = (value: number) => Math.max(0, value)
-
-const isBetween = (value: number, floor: number, ceil: number) =>
-  value >= floor && value <= ceil
+import { inRange, takeRight } from "lodash"
 
 const useCalculateScroll = (
   offset = majorScale(17)
 ): { activeTitle: string } => {
   const [activeTitle, setActiveTitle] = useState("")
 
-  const lastSectionTitles = [
-    analyticsEventSections[analyticsEventSections.length - 2].title,
-    analyticsEventSections[analyticsEventSections.length - 1].title,
-  ]
+  const lastSectionTitles = takeRight(analyticsEventSections, 2).map(
+    (section) => section.title
+  )
 
   useLayoutEffect(() => {
     const listener = () => {
-      const scroll = window.pageYOffset
+      const scrollPosition = window.pageYOffset
 
       const position = analyticsEventSections
         .map((eventSection) => {
           const { title } = eventSection
           const element = document.getElementById(title)
 
-          if (!element) return { title, top: -1, bottom: -1, rectTop: -1 }
+          if (!element) {
+            return { title, top: -1, bottom: -1, rectTop: -1 }
+          }
 
           const rect = element.getBoundingClientRect()
-          const top = clamp(rect.top + scroll - offset)
-          const bottom = clamp(rect.bottom + scroll - offset)
-
-          if (lastSectionTitles.includes(title)) {
-            console.log({
-              title,
-              top: rect.top,
-              bottom: rect.bottom,
-              scroll,
-              offset,
-            })
-          }
+          const top = Math.max(0, rect.top + scrollPosition - offset)
+          const bottom = Math.max(0, rect.bottom + scrollPosition - offset)
 
           return { title, top, bottom, rectTop: rect.top }
         })
@@ -48,7 +35,7 @@ const useCalculateScroll = (
           if (lastSectionTitles.includes(title)) {
             return rectTop > 0
           }
-          return isBetween(scroll, top, bottom)
+          return inRange(scrollPosition, top, bottom)
         })
 
       setActiveTitle(position?.title || "")
